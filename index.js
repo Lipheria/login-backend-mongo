11207549f09c3381d25e99d4aad51a5c3a86648f
Lipheria <64316193+Lipheria@users.noreply.github.com>
2023-09-24 09:21:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.get("/", (req, res) =>{
+    res.send("Hello Cheese")
+})
+
 mongoose.connect("mongodb://127.0.0.1:27017/plantidb")
 
 app.post("/login", (req, res) =>{
