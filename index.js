3286be44a06d63ac6dfc3ee14c76c0a68410c68d
Lipheria <64316193+Lipheria@users.noreply.github.com>
2023-09-26 04:20:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.post("/login", (req, res) =>{
 
 app.post("/register", async (req, res) => {
   console.log("Register API")
-    await userModel.save(req.body)
+    await userModel.create(req.body)
     .then(user => res.json(user))
     .catch(err => res.json(err))
 })
